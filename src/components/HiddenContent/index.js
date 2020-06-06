d3185df381ff158e9a8d3cb45a42641ce3e9a370
amangeldi <1998-amangeldi@mail.ru>
2020-06-06 17:40:50 +0600

    show hidden content only if age >=18

--- src/components/HiddenContent/index.js
+++ src/components/HiddenContent/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import { Boolean, func } from 'prop-types'
-import {hiddenContent} from '../../store/actions'
+import { number, func } from 'prop-types'
+import {hiddenContent, changeAge} from '../../store/actions'
 
-function HiddenContentShow ({showBlock, change}) {
-    // const [showBlock, setBlock] = React.useState(false)
+function HiddenContentShow ({showBlock, age, change}) {
     return (
         <div>
             <button onClick = {() => change(!showBlock)}>{showBlock ? 'hide' : 'show'}</button>
-            {showBlock && <div>HiddenContent</div>}
+            {showBlock && age >= 18 ? <div>HiddenContent</div> : null}
         </div>
     )
 }
 
 HiddenContentShow.propTypes = {
-    showBlock: Boolean,
-    change: func
+    age: number,
+    change: func,
 }
 const mapStateToProps = (state) => ({
-    showBlock: state.showBlock
+    showBlock: state.showBlock,
+    age: state.age,
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    change: (showBlock) => dispatch(hiddenContent(showBlock))
+    change: (showBlock) => dispatch(hiddenContent(showBlock)),
 })
+
+
 export default connect(mapStateToProps, mapDispatchToProps)(HiddenContentShow);
